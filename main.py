aca5c632deb4851c1bb279e1544be878e8a13b58
零式的天空 <29534255+DsTansice@users.noreply.github.com>
2026-05-03 15:44:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=b299650e-9f16-444e-9580-d542799dab87",
     # 添加更多链接
 ]
 
